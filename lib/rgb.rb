6aac1db202f3f427b6b4dc3c6fc0c542a6e55eea
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-23 09:10:00 +0900

    [update] refactoring of to_hex method

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,6 +1,11 @@
 def to_hex(r, g, b)
-  '#' +
-    r.to_s(16).rjust(2, '0') +
-    g.to_s(16).rjust(2, '0') +
-    b.to_s(16).rjust(2, '0')
+  [r, g, b].inject('#') do |hex, n|の戻り値になる
+    hex + n.to_s(16).rjust(2, '0')
+    # 繰り返し処理が完了したら、ブロックの戻り値がinjectメソッド自身の戻り値となる
+  end
+  # hex = '#'
+  # [r, g, b].each do |n|
+  #   hex += n.to_s(16).rjust(2, '0')
+  # end
+  # hex
 end
