460d022998c42e962c68fd8055deb03009c857e6
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-26 09:00:00 +0900

    [add] to_ints method

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,5 +1,5 @@
 def to_hex(r, g, b)
-  [r, g, b].inject('#') do |hex, n|の戻り値になる
+  [r, g, b].inject('#') do |hex, n|
     hex + n.to_s(16).rjust(2, '0')
     # 繰り返し処理が完了したら、ブロックの戻り値がinjectメソッド自身の戻り値となる
   end
@@ -8,4 +8,15 @@ def to_hex(r, g, b)
   #   hex += n.to_s(16).rjust(2, '0')
   # end
   # hex
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
 end
