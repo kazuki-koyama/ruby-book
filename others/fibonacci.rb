9d77b87f2b70e398b2781d37894d8b07ce824d0f
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-07-29 09:00:00 +0900

    [add] fibonacci sequence method

--- others/fibonacci.rb
+++ others/fibonacci.rb
@@ -1,3 +1,4 @@
+# N番目のフィボナッチ数を計算
 def fibonacci(n)
   return   if n < 0
   return n if n < 2
@@ -5,3 +6,14 @@ def fibonacci(n)
   n.times { a, b = b, a + b }
   a
 end
+
+# フィボナッチ数列を取得
+fib = Enumerator.new do |y|
+  a = b = 1
+  loop do
+    y << a
+    a, b = b, a + b
+  end
+end
+
+p fib.take(10)
