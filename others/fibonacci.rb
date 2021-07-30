89ac7d771d89cc8a0a03d4afce8d51e42a0ac5ab
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-07-30 09:00:00 +0900

    [add] tribonacci method

--- others/fibonacci.rb
+++ others/fibonacci.rb
@@ -7,13 +7,19 @@ def fibonacci(n)
   a
 end
 
-# フィボナッチ数列を取得
-fib = Enumerator.new do |y|
-  a = b = 1
-  loop do
-    y << a
-    a, b = b, a + b
-  end
-end
+puts "数を出したいのは何項目ですか？"
+n = gets.to_i  
+# -> 11を入力
+puts "#{n}項目の数字は#{fibonacci(n)}" 
+# -> 11項目の数字は89
 
-p fib.take(10)
+# フィボナッチ数列を取得
+# fib = Enumerator.new do |y|
+#   a = b = 1
+#   loop do
+#     y << a
+#     a, b = b, a + b
+#   end
+# end
+
+# p fib.take(10)
